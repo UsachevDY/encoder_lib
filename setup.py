11a3c36a2ec96464ce3270577a9d39df502bf9db
Denis Usachev <usachevdy@yandex.ru>
2020-05-10 20:52:48 +0300

    Updated install script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='encoder_lib',
-    version='1.0',
+    version='1.1',
     author="Denis Usachev",
     author_email="usachevdy@yandex.ru",
     url="https://github.com/NightFantom/encoder_lib",
